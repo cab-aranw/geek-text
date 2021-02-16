79f3a2a4c1ac4a5a6291bd5e20b83c1e4f108bc4
Lyceum <48872358+cab-aranw@users.noreply.github.com>
2021-02-15 22:32:46 -0500

    Updating USer Collection

--- geek-text-backend/models/author.model.js
+++ geek-text-backend/models/author.model.js
@@ -1,1 +1,42 @@
 //TODO
+import {Schema,models} from 'mongoose'
+const user = new Schema ({
+    username : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+    password :{
+        type : String,
+        required : true,
+        unique : true,
+    },
+    emailAddress : {
+
+        type : String,
+        required : true,
+        unique : true,
+    },
+    homeAddress : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+    homeAddressValidation : {
+        type : Boolean,
+        required :true,
+    },
+    nickname : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+    anonymousStatus :{
+        type : Boolean,
+        required : true,
+    },
+
+}),
+
+const user = mongoose.model('User',UserSchema);
+module.exports = User;
