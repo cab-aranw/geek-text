28794a6d6820248d25b027862c9a9b60e9b3c3d2
azhareus <azhar.hj@gmail.com>
2021-02-18 12:56:51 -0500

    Added to  author model

--- geek-text-backend/models/author.model.js
+++ geek-text-backend/models/author.model.js
@@ -1,1 +1,23 @@
-//TODO
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const authorSchema = new Schema({
+    name: {
+        type: String,
+        maxlength: 50,
+        required: true
+    }, 
+    bio: {
+        type: String,
+        maxlength: 300,
+    }, 
+    books:{
+        type: [Schema.Types.ObjectId],
+        ref: 'Book',
+        default: []
+    },
+});
+
+const Author = mongoose.model('Author', authorSchema);
+module.exports = Author;
